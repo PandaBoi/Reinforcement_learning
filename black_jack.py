66f641d592b953fa2e34db48d80fe83848452eb6
Rohan Panda <rohanpanda.99@gmail.com>
2019-06-22 02:30:43 +0530

    Add: MC control e-greedy method

--- black_jack.py
+++ black_jack.py
@@ -8,25 +8,36 @@ sys.path.append("../")
 from modified_env import Black_jack
 from collections import defaultdict
  
-
+#with inspiration from https://github.com/dennybritz/reinforcement-learning/
 class Agent():
 
-	def __init__(self,env,discount = 1.0,episodes = 10000):
+	def __init__(self,env,discount = 1.0,epsilon = 0.1,episodes = 10000):
 
 		self.env = env
 		self.discount = discount
 		self.actions = [0,1]
+		self.epsilon = epsilon
 		self.episodes = episodes
 		self.state_count = defaultdict(float)
-
-		self.State_values = defaultdict(float)
-		self.State_values2 = defaultdict(float)
+		self.sa_values = defaultdict(float)
 		self.state_sum = defaultdict(float)
+		self.State_values = defaultdict(float)
 
-	def player_policy(self,state):
+	def player_policy(self,state,first_time = True):
 
-		sum_hand , dealer_hand, ace = state
-		return 0 if sum_hand >= 20 else 1
+		if first_time:
+			sum_hand , dealer_hand, ace = state
+			return 0 if sum_hand >= 20 else 1
+		else:
+			num = np.random.random()
+			if(num <self.epsilon):
+				action = np.random.choice(self.actions, p = [0.5,0.5])
+			else:
+				q_vals = [self.sa_values[(state,x)] for x in self.actions]
+				action = np.argmax(q_vals)
+
+			return action
+
 
 	
 	
@@ -60,31 +71,110 @@ class Agent():
 
 				if state_p not in self.State_values.keys():
 					self.State_values[state_p] = 0
-					self.State_values2[state_p] = 0
 					self.state_count[state_p] =0
-					self.state_sum[state_p] = 0
 				
 				self.state_count[state_p] +=1
 				self.state_sum[state_p] +=G
 				self.State_values[state_p] += (G - self.State_values[state_p])/(self.state_count[state_p] )
-				self.State_values2[state_p] = (self.state_sum[state_p])/(self.state_count[state_p])
+
+
+	def MC_control(self):
+		first_time = True
+		for i in range(self.episodes+1):
+
+			if i%1000 ==0:
+				print("episode {}/{} done".format(i,self.episodes))
+
+			ep = []
+			env = self.env
+			state = env.reset()
+			done = False
+			
+			while not done:
+				action = self.player_policy(state,first_time)
+				next_state , reward , done,_ = env.step(action)
+				#appending action as it is needed
+				#for estimation of  the state_action values
+				ep.append((state,action,reward))
+				state = next_state
+			first_time = False
+
+			ep_states_action = set([(tuple(x[0]),x[1]) for x in ep])
+			# G =0
+			for state,action in ep_states_action:
+				st_ac = (state,action)
+				first_visit = [i for i,q in enumerate(ep) if q[0]==state and q[1] == action][0]
+				G = sum([re[2] *(self.discount** i) for i,re in enumerate(ep[first_visit:])])
+
+				if st_ac not in self.sa_values.keys():
+					self.sa_values[st_ac] = 0
+					self.state_count[st_ac] =0
+				
+				self.state_count[st_ac] +=1
+				self.state_sum[st_ac] +=G
+				self.sa_values[st_ac] += (G - self.sa_values[st_ac])/(self.state_count[st_ac] )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 env = Black_jack.BlackjackEnv()
-steps = 50000
-test = Agent(env,episodes = steps)
-test.estimation_step()
-noace = list(filter(lambda x: (x[2]== False) , test.State_values.keys() ))
-ace = list(filter(lambda x: (x[2]== True) , test.State_values.keys() ))
+steps = 100000
+test = Agent(env,episodes = steps, epsilon = 0.2)
 
-noace_val = [(test.State_values[x]) for x in noace]
-ace_val = [(test.State_values[x]) for x in ace]
-keys =list(test.State_values.keys())
+# print(test.sa_values)
 
-x1 = min(key[0] for key in keys)
-x2 = max(key[0] for key in keys)
-y1 = min(key[1] for key in keys)
-y2 = max(key[1] for key in keys)
+#for estimation
+# test.estimation_step()
+# noace = list(filter(lambda x: (x[2]== False) , test.State_values.keys() ))
+# ace = list(filter(lambda x: (x[2]== True) , test.State_values.keys() ))
+
+
+
+# noace_val = [(test.State_values[x]) for x in noace]
+# ace_val = [(test.State_values[x]) for x in ace]
+# keys =list(test.State_values.keys())
+
+# x1 = min(key[0] for key in keys)
+# x2 = max(key[0] for key in keys)
+# y1 = min(key[1] for key in keys)
+# y2 = max(key[1] for key in keys)
+# x_a = range(x1,x2+1)
+# y_a = range(y1,y2+1)
+# X,Y= np.meshgrid(x_a,y_a)
+# Z_noace = np.apply_along_axis(lambda _: test.sa_values[(_[0], _[1], False)], 2, np.dstack([X, Y]))
+# Z_ace = np.apply_along_axis(lambda _: test.sa_values[(_[0], _[1], True)], 2, np.dstack([X, Y]))
+
+
+#for control
+
+test.MC_control()
+noace = list(filter(lambda x: (x[0][2]== False) , test.sa_values.keys() ))
+ace = list(filter(lambda x: (x[0][2]== True) , test.sa_values.keys() ))
+
+
+noace_val = [(test.sa_values[x]) for x in noace]
+ace_val = [(test.sa_values[x]) for x in ace]
+keys =list(test.sa_values.keys())
+# print(ace_val)
+
+x1 = min(key[0][0] for key in keys)
+x2 = max(key[0][0] for key in keys)
+y1 = min(key[0][1] for key in keys)
+y2 = max(key[0][1] for key in keys)
 x_a = range(x1,x2+1)
 y_a = range(y1,y2+1)
 X,Y= np.meshgrid(x_a,y_a)
@@ -100,11 +190,12 @@ def make_graph_3d(X,Y,Z,title):
 	ax.set_title(title)
 	plt.show()
 
-noace_val =np.array(noace_val)[:,np.newaxis]
-Z_noace = np.apply_along_axis(lambda _: test.State_values[(_[0], _[1], False)], 2, np.dstack([X, Y]))
-Z_ace = np.apply_along_axis(lambda _: test.State_values[(_[0], _[1], True)], 2, np.dstack([X, Y]))
+Z_noace = np.apply_along_axis(lambda _: test.sa_values[(_[0], _[1], False),1], 2, np.dstack([X, Y]))
+Z_noace = np.apply_along_axis(lambda _: test.sa_values[(_[0], _[1], False),0], 2, np.dstack([X, Y]))
+Z_ace = np.apply_along_axis(lambda _: test.sa_values[(_[0], _[1], True),1], 2, np.dstack([X, Y]))
+Z_ace = np.apply_along_axis(lambda _: test.sa_values[(_[0], _[1], True),0], 2, np.dstack([X, Y]))
 
-print(len(Y))
+# print(len(Y))
 
-make_graph_3d(X, Y, Z_noace,'{} (no usable as)'.format(steps))
-make_graph_3d(X, Y, Z_ace,'{} (usable as)'.format(steps))
+make_graph_3d(X, Y, Z_noace,'{} (no usable ace)'.format(steps))
+make_graph_3d(X, Y, Z_ace,'{} (usable ace)'.format(steps))
